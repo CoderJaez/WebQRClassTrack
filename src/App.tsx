b37c938a9dbf7ee5406bf8b509699a11612e11c7
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-09-19 06:52:00 +0800

    Add components, files, folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import { useState } from "react";
-import "./App.css";
-import useBearStore from "./store/bear";
 
 function App() {
-  const { bears, increasePopulation } = useBearStore();
-  const bears2 = useBearStore.getState();
-  // const increment = useBearStore.setState((state) => ({
-  //   bears: state.bears + 2,
-  // }));
-  return (
-    <>
-      <p>Bears count: {bears}</p>
-      <button onClick={increasePopulation}>Increment</button>
-      <p>Bears 2: {bears2.bears}</p>
-      <button onClick={bears2.increasePopulation}>Increment bear 2</button>
-
-      {/* <button onClick={() => increment}> Increment +2 </button> */}
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { PrimeReactProvider } from "primereact/api";
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+//core
+import "primereact/resources/primereact.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
-)
+);
