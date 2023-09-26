80363a7de6e84e31f5b93dc712c68e577d3fdfe1
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2023-09-26 08:31:50 +0800

    Add module: UserServices, addUser function

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -11,11 +11,83 @@ import {
   Table,
   PasswordInput,
   TextInput,
+  Select,
   Stack,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { UserInfo } from "types";
+import useUser from "@services/UserService";
+
 const UserPage: React.FC = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const { loading, addUser } = useUser();
+
+  const handleSubmit = async (values: Omit<UserInfo, "id" | "image_path">) => {
+    //Do nothing yet.
+    await addUser(values)
+      .then((res) => {
+        toast.success(res.message as string);
+      })
+      .catch((err) => {
+        if (err.status === 400) {
+          err.message.forEach((msg: any) =>
+            form.setFieldError(msg.field, msg.message),
+          );
+        } else {
+          toast.error(err.message as string);
+        }
+      });
+  };
+
+  const form = useForm<Omit<UserInfo, "id" | "image_path">>({
+    validateInputOnChange: true,
+    initialValues: {
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      email: "",
+      password: "",
+      role: "",
+      contact_no: "",
+    },
+    validate: {
+      firstname: (value) =>
+        value === undefined || value === ""
+          ? "Firstname is required"
+          : value.length < 2
+          ? "Must at least 2 characters."
+          : null,
+      lastname: (value) =>
+        value === undefined || value === ""
+          ? "Lastname is required"
+          : value.length < 2
+          ? "Must at least 2 characters."
+          : null,
+      contact_no: (value) =>
+        value === undefined || value.trim() === ""
+          ? "Contact number is required"
+          : value.length < 11 || value.length > 11
+          ? "Invalid contact number"
+          : !/^09\d*$/.test(value)
+          ? "Invalid contact number"
+          : null,
+      email: (value) =>
+        value === undefined || value.trim() === ""
+          ? "Email is required"
+          : !/^\S+@\S+$/.test(value)
+          ? "Invalid email"
+          : null,
+      password: (value) =>
+        value === undefined || value.trim() === ""
+          ? "Password is required"
+          : value.length < 8
+          ? "Must at least 8 characters"
+          : null,
+      role: (value) =>
+        value === undefined || value === null ? "User Type is required" : null,
+    },
+  });
 
   return (
     <Container fluid p="lg">
@@ -26,7 +98,7 @@ const UserPage: React.FC = () => {
             Users
           </Title>
           <Group justify="space-between" m="md">
-            <Button onClick={open}>New Classroom</Button>
+            <Button onClick={open}>Add new user</Button>
             <TextInput
               placeholder="Search"
               // onKeyDown={onSearchHandler}
@@ -57,19 +129,42 @@ const UserPage: React.FC = () => {
         closeOnClickOutside={false}
         closeOnEscape
       >
-        <form>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
           <Stack>
-            <TextInput label="Firstname" placeholder="Eg. John" />
-            <TextInput label="Middlenane" placeholder="Eg. M" />
-            <TextInput label="Lastname" placeholder="Eg. Doe" />
-            <TextInput label="Contact No" placeholder="Eg. 0912456077" />
-            <TextInput label="Email" placeholder="Eg. youremail@gmail.com" />
-            <PasswordInput label="Password" placeholder="Enter password" />
-            <PasswordInput
-              label="Confirm Password"
-              placeholder="Re-enter password"
+            <TextInput
+              label="Firstname"
+              placeholder="Eg. John"
+              {...form.getInputProps("firstname")}
             />
-            <Button type="submit">Save</Button>
+            <TextInput label="Middlenane" placeholder="Eg. M" />
+            <TextInput
+              label="Lastname"
+              placeholder="Eg. Doe"
+              {...form.getInputProps("lastname")}
+            />
+            <TextInput
+              label="Contact No"
+              placeholder="Eg. 0912456077"
+              {...form.getInputProps("contact_no")}
+            />
+            <TextInput
+              label="Email"
+              placeholder="Eg. youremail@gmail.com"
+              {...form.getInputProps("email")}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Enter password"
+              {...form.getInputProps("password")}
+            />
+            <Select
+              label="User type"
+              data={["Admin", "Instructor"]}
+              {...form.getInputProps("role")}
+            />
+            <Button type="submit" loading={loading}>
+              Save
+            </Button>
           </Stack>
         </form>
       </Modal>
---
+++ src/services/UserService.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import useAxiosPrivate from "@hooks/useAxiosPrivate";
+import { UserInfo, Response } from "types";
+import { resolve } from "path";
+import { rejects } from "assert";
+
+interface UserResponse extends Response {
+  user: UserInfo | null;
+}
+
+const useUser = () => {
+  const [loading, setLoading] = useState(false);
+  const axios = useAxiosPrivate();
+
+  const addUser = async (values: Omit<UserInfo, "id" | "image_path">) => {
+    setLoading(true);
+    const value = { ...values, ...{ role: values.role.toLowerCase() } };
+    return await new Promise<UserResponse>((resolve, reject) => {
+      axios
+        .post("users", value)
+        .then((res) => {
+          const result: UserResponse = {
+            status: res.status,
+            user: res.data.data,
+            message: res.data.message,
+          };
+          resolve(result);
+        })
+        .catch((err) => {
+          const result: UserResponse = {
+            status: err.response.status,
+            user: null,
+            message: err.response.data.message,
+          };
+          reject(result);
+        })
+        .finally(() => setLoading(false));
+    });
+  };
+
+  return {
+    loading,
+    addUser,
+  };
+};
+
+export default useUser;
